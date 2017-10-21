1b05e972024944cad8dbb4f0642636d0326b790c
Maik Marschner <maik@maik.tk>
2017-10-21 20:46:19 +0200

    feat(resolver): Accept cwd from arguments.
    * Don't use process.cwd() to resolve module files.
    * Lint and refactor the code.

--- src/resolver.js
+++ src/resolver.js
@@ -6,12 +6,12 @@ const Module = require("module");
  * @param {String} fileName
  * @returns {String|null}
  */
-function resolveFilename(fileName) {
+function resolveFilename(fileName, relativeTo = process.cwd()) {
   try {
     const parent = new Module();
 
     // eslint-disable-next-line no-underscore-dangle
-    parent.paths = Module._nodeModulePaths(process.cwd());
+    parent.paths = Module._nodeModulePaths(relativeTo);
 
     // eslint-disable-next-line no-underscore-dangle
     return Module._resolveFilename(fileName, parent);
