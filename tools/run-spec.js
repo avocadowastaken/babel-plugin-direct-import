730b2fc7eab991e4073ef448773ad82f5aeaf66b
Umidbek Karimov <uma.karimov@gmail.com>
2018-12-17 15:51:32 +0500

    refactor(run-spec): Rewrite without lodash.

--- tools/run-spec.js
+++ tools/run-spec.js
@@ -7,8 +7,6 @@ const babel = require("babel-core");
 const plugin = require("../src/index");
 const { fulfillConfigExports } = require("../src/mapper");
 
-const fp = require("lodash/fp");
-
 const RAW = Symbol("raw");
 
 function makeRaw(input, output) {
@@ -17,19 +15,20 @@ function makeRaw(input, output) {
   };
 }
 
-const safeReadDir = dir => {
+function safeReadDir(dir) {
   try {
     return fs.readdirSync(dir).map(x => path.join(dir, x));
   } catch (e) {
     return [];
   }
-};
+}
 
-const getFixtures = fp.flow(
-  x => [[x, "error"], [x, "transform"]],
-  fp.map(([x, dir]) => [dir, safeReadDir(path.join(x, dir))]),
-  fp.fromPairs
-);
+function getFixtures(testDir) {
+  return {
+    error: safeReadDir(path.join(testDir, "error")),
+    transform: safeReadDir(path.join(testDir, "transform")),
+  };
+}
 
 function runSpec(dir, spec) {
   const [version, pkg] = dir.split(path.sep).reverse();
