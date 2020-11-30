7461a9d51ea8126c49e15899dd608fa105168541
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2020-11-30 12:15:58 +0500

    test: Check `@material-ui/lab`.

---
+++ src/__tests__/@material-ui/lab/plugin.spec.ts
@@ -0,0 +1,14 @@
+import { runPlugin } from '../../../__testutils__/runPlugin';
+
+test('transforms', () => {
+  expect(
+    runPlugin(
+      'import { Autocomplete, createFilterOptions, useAutocomplete } from "@material-ui/lab"',
+      ['@material-ui/lab'],
+    ),
+  ).toMatchInlineSnapshot(`
+    import Autocomplete from "@material-ui/lab/esm/Autocomplete/index.js";
+    import { createFilterOptions } from "@material-ui/lab/esm/Autocomplete/Autocomplete.js";
+    import useAutocomplete from "@material-ui/lab/esm/useAutocomplete/index.js";
+  `);
+});
