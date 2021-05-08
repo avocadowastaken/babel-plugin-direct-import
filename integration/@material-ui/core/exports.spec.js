af1f08b4988c0001d6620697e795647602c8dd35
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-05-08 11:31:43 +0500

    refactor: [resolvePackageEntry] extract from `getModuleExports`.

--- integration/@material-ui/core/exports.spec.js
+++ integration/@material-ui/core/exports.spec.js
@@ -4,5 +4,4 @@ const getModuleExports = require("../../../lib/internal/getModuleExports");
 
 it("resolves exports for `@material-ui/core`", () => {
   expect(getModuleExports("@material-ui/core")).toMatchSnapshot();
-  expect(getModuleExports("@material-ui/core", "es/index")).toMatchSnapshot();
 });
