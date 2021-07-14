5b0b9f0c86f49c2d975d5a8c5e864b2234e959c5
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-07-15 03:15:40 +0500

    chore: Bump dependencies.

--- test/luxon/exports.spec.js
+++ test/luxon/exports.spec.js
@@ -4,62 +4,62 @@ const testExports = require("../testExports");
 
 test("exports", () => {
   expect(testExports("luxon")).toMatchInlineSnapshot(`
-    Array [
-      Array [
-        "DateTime",
-        "default",
-        "luxon/src/datetime.js",
-      ],
-      Array [
-        "Duration",
-        "default",
-        "luxon/src/duration.js",
-      ],
-      Array [
-        "Info",
-        "default",
-        "luxon/src/info.js",
-      ],
-      Array [
-        "Interval",
-        "default",
-        "luxon/src/interval.js",
-      ],
-      Array [
-        "VERSION",
-        "VERSION",
-        "luxon/src/luxon.js",
-      ],
-      Array [
-        "Settings",
-        "default",
-        "luxon/src/settings.js",
-      ],
-      Array [
-        "Zone",
-        "default",
-        "luxon/src/zone.js",
-      ],
-      Array [
-        "FixedOffsetZone",
-        "default",
-        "luxon/src/zones/fixedOffsetZone.js",
-      ],
-      Array [
-        "IANAZone",
-        "default",
-        "luxon/src/zones/IANAZone.js",
-      ],
-      Array [
-        "InvalidZone",
-        "default",
-        "luxon/src/zones/invalidZone.js",
-      ],
-      Array [
-        "LocalZone",
-        "default",
-        "luxon/src/zones/localZone.js",
-      ],
-    ]
-  `);
+Array [
+  Array [
+    "DateTime",
+    "default",
+    "luxon/src/datetime.js",
+  ],
+  Array [
+    "Duration",
+    "default",
+    "luxon/src/duration.js",
+  ],
+  Array [
+    "Info",
+    "default",
+    "luxon/src/info.js",
+  ],
+  Array [
+    "Interval",
+    "default",
+    "luxon/src/interval.js",
+  ],
+  Array [
+    "VERSION",
+    "VERSION",
+    "luxon/src/luxon.js",
+  ],
+  Array [
+    "Settings",
+    "default",
+    "luxon/src/settings.js",
+  ],
+  Array [
+    "Zone",
+    "default",
+    "luxon/src/zone.js",
+  ],
+  Array [
+    "FixedOffsetZone",
+    "default",
+    "luxon/src/zones/fixedOffsetZone.js",
+  ],
+  Array [
+    "IANAZone",
+    "default",
+    "luxon/src/zones/IANAZone.js",
+  ],
+  Array [
+    "InvalidZone",
+    "default",
+    "luxon/src/zones/invalidZone.js",
+  ],
+  Array [
+    "SystemZone",
+    "default",
+    "luxon/src/zones/systemZone.js",
+  ],
+]
+`);
 });
