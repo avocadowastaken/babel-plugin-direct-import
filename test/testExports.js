8c327ba0543767182e29df40a6e2ed5493b9e501
Umidbek <4734297+umidbekk@users.noreply.github.com>
2021-10-05 01:19:09 +0300

    chore: Setup `@umidbekk/configs`.

--- test/testExports.js
+++ test/testExports.js
@@ -11,9 +11,13 @@ const DependencyTree = require("../lib/internal/DependencyTree");
  */
 module.exports = function testExports(id) {
   const tree = DependencyTree.create(NodeModule.get(id), babel);
-  return Array.from(tree.getDependencies().values(), (dependency) => [
-    dependency.id,
-    dependency.internalID,
-    dependency.source,
-  ]).sort(([, , a], [, , b]) => a.localeCompare(b));
+
+  /** @type {Array<[id: string, internalID: string, source: string]>} */
+  const result = [];
+
+  for (const [id, { source, internalID }] of tree.getDependencies()) {
+    result.push([id, internalID, source]);
+  }
+
+  return result.sort(([, , a], [, , b]) => a.localeCompare(b));
 };
